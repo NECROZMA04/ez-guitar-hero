0478e05e416aaf99f8ca5031035e78fb093ffc84
NECROZMA04 <faraaz.rasool.azaz12@gmail.com>
2025-09-06 14:42:13 +0800

    refactor: adjust project structure for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import pluginChecker from "vite-plugin-checker";
 import { UserConfig } from "vite";
 
 const config: UserConfig = {
+    base: "/ez-guitar-hero/",
     plugins: [pluginChecker({ typescript: true, overlay: false })],
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+            },
+        },
+    },
 };
 
 const getConfig = () => config;
